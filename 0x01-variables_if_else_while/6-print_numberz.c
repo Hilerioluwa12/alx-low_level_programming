e5a7ec89c96e296c894a4ef1abee4ba792aabad6
hilerioluwa12 <hardexilerioluwa@gmail.com>
2022-07-03 07:15:15 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print 0 - 9 digit using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
