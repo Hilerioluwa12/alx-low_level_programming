5eca6c1f4d418f7348df236ee3bc44e0271359e4
hilerioluwa12 <hardexilerioluwa@gmail.com>
2022-07-03 12:33:29 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - printing combination of two numbers 01 to 99.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a, b;
+
+	a = 48;
+	b = 48;
+
+	while (b < 58)
+	{
+		a = 48;
+		while ( a < 58)
+		{
+			putchar(b);
+			putchar(a);
+			if (a == 57 && b == 57)
+			{
+				break;
+			}
+			putchar(',');
+			putchar(' ');
+			a++;
+		}
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
