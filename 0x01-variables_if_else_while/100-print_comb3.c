6df8c695c4e44dfe9ccb60a85dbe8ea79cab768a
hilerioluwa12 <hardexilerioluwa@gmail.com>
2022-07-03 13:25:55 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,63 +7,18 @@
  */
 int main(void)
 {
-	int a, b;
+	int a;
 
-	a = 49;
-	b = 48;
-
-	while (b < 57)
+	for (a = 0; a <= 89; a++)
+	if (a < ((a % 10)  * 10) + (a / 10))
 	{
-		a = 49;
-		while (a < 58)
-		{
-			putchar(b);
-			putchar(a);
-			if (a == 48 && b == 48)
-			{
-				break;
-			}
-			else if (a == 49 && b == 49)
-			{
-				break;
-			}
-			else if (a == 50 && b == 50)
-			{
-				break;
-			}
-			else if (a == 51 && b == 51)
-			{
-				break;
-			}
-			else if (a == 52 && b == 52)
-			{
-				break;
-			}
-			else if (a == 53 && b == 53)
-			{
-				break;
-			}
-			else if (a == 54 && b == 54)
-			{
-				break;
-			}
-			else if (a == 55 &&  b == 55)
-			{
-				break;
-			}
-			else if (a == 56 && b == 56)
-			{
-				break;
-			}
-			else if (a == 57 && b == 56)
-			{
-				break;
-			}
-			putchar(',');
-			putchar(' ');
-			a++;
-		}
-		b++;
+	putchar(a / 10 + '0');
+	putchar(a % 10 + '0');
+	if (a < 89)
+	{
+		putchar(',');
+		putchar(' ');
+	}
 	}
 	putchar('\n');
 	return (0);
