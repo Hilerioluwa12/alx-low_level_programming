e0cfac198dfce0cc510308a2c8380b49f87eb3dc
hilerioluwa12 <hardexilerioluwa@gmail.com>
2022-07-03 06:25:43 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Printing the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ahp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int b;
+
+	for (b = 0; b > 26; b++)
+	{
+		putchar(ahp[b]);
+	}
+	putchar('\n');
+	return (0);
+}
