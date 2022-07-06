64ecfa7d06d850e18075f781900119c23842c6e1
hilerioluwa12 <hardexilerioluwa@gmail.com>
2022-07-05 20:28:01 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#include "main.h"
+
+/**
+*print_alphabet - display character
+*Return: 0
+*/
+
+void print_alphabet(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
