b7b920f95d85a373377d8fba33febba18e568d67
hilerioluwa12 <hardexilerioluwa@gmail.com>
2022-07-07 01:57:45 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,17 +3,16 @@
 /**
  * main - program that print the first 50 fibonacci number
  *
- * Return - Always 0 success
+ * Return: Always 0 success
  */
-int main (void)
+int main(void)
 {
 	int a;
 	long int c, e, fn;
 
 	c = 1;
 	e = 2;
-	printf("%ld, ", c);
-	printf("%ld, ", e);
+	printf("%ld, %ld", c, e);
 	for (a = 0; a < 48; a++)
 	{
 		fn = c + e;
