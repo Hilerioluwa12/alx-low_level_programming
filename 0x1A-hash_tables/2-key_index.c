05d173ddd52532ac05700e9ccbed9b0a92a8e20e
hilerioluwa12 <hardexilerioluwa2gmail.com>
2022-10-21 20:44:40 -0700

    1-djb2.c

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ *key_index - function that the index of a given key
+ *@key: a pointer to a key
+ *@size: the size of hash table
+ *
+ *Return: the index of key
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned int long i;
+
+	i = hash_djb2(key);
+	return (i % size);
+}
